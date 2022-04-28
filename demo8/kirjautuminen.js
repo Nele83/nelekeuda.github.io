3acd2c95039c3d1a8420afd47db9e15c6125ce13
Nele83 <98733060+Nele83@users.noreply.github.com>
2022-04-28 12:50:22 +0300

    luotu demo8
    muokattu

--- demo8/kirjautuminen.js
+++ demo8/kirjautuminen.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", kirjaudu);
 
-function kirjaudu(){
+function onkokirjautunut(){
     if(localStorage.getItem("kirjautunut") === 'kylla'){
         document.getElementById('tervetuloa_teksti').textContent += localStorage.getItem("nimi");
         document.getElementById("kirjautumis_lomake").style.display= "none";
+    }
+    else {
+        document.getElementById("ulos kirjautuminen").compareDocumentPosition.display="none";
     }
 }
 function kirjaudu(){
     localStorage.setItem("nimi", document.getElementById("nimi").value);
     localStorage.setItem("kirjautunut", "kylla");
+}
+function kirjaudu_ulos(){
+    localStorage.setItem("kirjautunut", "ei");
 }
