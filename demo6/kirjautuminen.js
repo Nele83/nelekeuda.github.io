86f24102629568cebca0c0a53d440e728ca05762
Nele83 <98733060+Nele83@users.noreply.github.com>
2022-04-28 09:26:01 +0300

    2022

--- demo6/kirjautuminen.js
+++ demo6/kirjautuminen.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 function onkoKirjautunut(){
     let kirjautunut = 'kylla';
     if(kirjautunut === 'kylla'){
-        document.getElementById('tervetuloa_teksti').textContent += ' Nele!Eip ';
+        document.getElementById('tervetuloa_teksti').textContent += ' Nele Moi! ';
     }
 }
