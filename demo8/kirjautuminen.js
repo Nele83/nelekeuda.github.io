92f6deca9517cc99ce7b9811ed24a559f6e652ca
Nele83 <98733060+Nele83@users.noreply.github.com>
2022-04-28 15:59:37 +0300

    demo8
    Sain toimimaan

--- demo8/kirjautuminen.js
+++ demo8/kirjautuminen.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", kirjaudu);
+document.addEventListener("DOMContentLoaded", onkokirjautunut);
 
 function onkokirjautunut(){
-    if(localStorage.getItem("kirjautunut") === 'kylla'){
-        document.getElementById('tervetuloa_teksti').textContent += localStorage.getItem("nimi");
-        document.getElementById("kirjautumis_lomake").style.display= "none";
-    }
-    else {
-        document.getElementById("ulos kirjautuminen").compareDocumentPosition.display="none";
-    }
+ if (localStorage.getItem("kirjautunut") === 'kylla'){
+     document.getElementById('tervetulo_teksti').textContent += localStorage.getItem("nimi");
+     document.getElementById("kirjautumis_lomake").style.display = "none";
+ }
+else {
+    document.getElementById("ulos_kirjautuminen").style.display = "none";
+}
 }
 function kirjaudu(){
     localStorage.setItem("nimi", document.getElementById("nimi").value);
     localStorage.setItem("kirjautunut", "kylla");
 }
-function kirjaudu_ulos(){
+function kirjaudu_ulos() {
     localStorage.setItem("kirjautunut", "ei");
 }
